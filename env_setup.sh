9d0ff6e3ea35c0d801bdf8b9148a23b1aa9e6e95
marcin.rzucidlo <marcin.rzucidlo@yupo.pl>
2020-10-19 13:55:26 +0200

    build paralely with proper tag

--- env_setup.sh
+++ env_setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "GO_VERSION=^1.15.2" >>$GITHUB_ENV
 echo "PROJECT_ID=projectvoltron" >>$GITHUB_ENV
-#echo "DOCKER_TAG=$(echo ${GITHUB_SHA:0:7})" >>$GITHUB_ENV
-#echo "PR_NUMBER=${{ github.event.number }}" >>$GITHUB_ENV
 echo "APPS=gateway k8s-engine och" >>$GITHUB_ENV
 
 if [ "${GITHUB_EVENT_NAME}" = "push" ]
@@ -11,5 +9,3 @@ then
 else
   echo "DOCKER_TAG=PR-$(echo ${PR_NUMBER})" >>$GITHUB_ENV
 fi
-# PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
-# [ -z "${PR_NUMBER}"] && echo "DOCKER_TAG=$(echo ${GITHUB_SHA:0:7})" >>$GITHUB_ENV || echo "DOCKER_TAG=PR-$(echo ${PR_NUMBER})" >>$GITHUB_ENV
