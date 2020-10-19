1ceed201063d66292167a674978d959f4a913476
marcin.rzucidlo <marcin.rzucidlo@yupo.pl>
2020-10-19 13:49:01 +0200

    build paralelly

--- env_setup.sh
+++ env_setup.sh
@@ -5,5 +5,11 @@ echo "PROJECT_ID=projectvoltron" >>$GITHUB_ENV
 #echo "PR_NUMBER=${{ github.event.number }}" >>$GITHUB_ENV
 echo "APPS=gateway k8s-engine och" >>$GITHUB_ENV
 
-PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
-[ -z "${PR_NUMBER}"] && echo "DOCKER_TAG=$(echo ${GITHUB_SHA:0:7})" >>$GITHUB_ENV || echo "DOCKER_TAG=PR-$(echo ${PR_NUMBER})" >>$GITHUB_ENV
+if [ "${GITHUB_EVENT_NAME}" = "push" ]
+then
+  echo "DOCKER_TAG=$(echo ${GITHUB_SHA:0:7})" >>$GITHUB_ENV
+else
+  echo "DOCKER_TAG=PR-$(echo ${PR_NUMBER})" >>$GITHUB_ENV
+fi
+# PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
+# [ -z "${PR_NUMBER}"] && echo "DOCKER_TAG=$(echo ${GITHUB_SHA:0:7})" >>$GITHUB_ENV || echo "DOCKER_TAG=PR-$(echo ${PR_NUMBER})" >>$GITHUB_ENV
