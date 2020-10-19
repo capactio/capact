4d470bf442bf2f2d9acc4d38b1a822827b769e20
marcin.rzucidlo <marcin.rzucidlo@yupo.pl>
2020-10-19 15:53:26 +0200

    PR tag & display docker build logs

--- env_setup.sh
+++ env_setup.sh
@@ -7,5 +7,6 @@ if [ "${GITHUB_EVENT_NAME}" = "push" ]
 then
   echo "DOCKER_TAG=$(echo ${GITHUB_SHA:0:7})" >>$GITHUB_ENV
 else
+  PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
   echo "DOCKER_TAG=PR-$(echo ${PR_NUMBER})" >>$GITHUB_ENV
 fi
