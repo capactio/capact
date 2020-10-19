05c0075ce2807b68d51343242f7e0c0ac931c525
marcin.rzucidlo <marcin.rzucidlo@yupo.pl>
2020-10-19 12:35:08 +0200

    single service

--- env_setup.sh
+++ env_setup.sh
@@ -2,4 +2,4 @@ echo "GO_VERSION=^1.15.2" >>$GITHUB_ENV
 echo "PROJECT_ID=projectvoltron" >>$GITHUB_ENV
 echo "DOCKER_TAG=$(echo ${GITHUB_SHA:0:7})" >>$GITHUB_ENV
 #echo "PR_NUMBER=${{ github.event.number }}" >>GITHUB_ENV
-echo "export APPS = gateway k8s-engine och" >>GITHUB_ENV
+echo "APPS=gateway k8s-engine och" >>GITHUB_ENV
