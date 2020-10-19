fc7ba10368796f3886c9ac16ed1a7663b707e1da
Pawel Kosiec <pawel@structsure.co>
2020-10-19 14:42:09 +0200

    Fix Go struct generation for metrics dashboard item

--- pkg/sdk/apis/0.0.1/types/types.gen.go
+++ pkg/sdk/apis/0.0.1/types/types.gen.go
@@ -24,8 +24,6 @@
 
 package types
 
-import "bytes"
-import "errors"
 import "encoding/json"
 
 func UnmarshalInterface(data []byte) (Interface, error) {
@@ -383,12 +381,13 @@ type Health struct {
 
 // Details about metrics exposed for a given TypeInstance
 type Metrics struct {
-	Dashboards []DashboardElement `json:"dashboards,omitempty"`// Metrics dashboards
-	Regex      *string            `json:"regex,omitempty"`     // Regex for exposed metrics
-	URL        *string            `json:"url,omitempty"`       // URL of metrics endpoint
+	Dashboards []Dashboard `json:"dashboards,omitempty"`// Metrics dashboards
+	Regex      *string     `json:"regex,omitempty"`     // Regex for exposed metrics
+	URL        *string     `json:"url,omitempty"`       // URL of metrics endpoint
 }
 
-type DashboardClass struct {
+// Stores details of a metrics dashboard
+type Dashboard struct {
 	URL *string `json:"url,omitempty"`// URL of metrics dashboard
 }
 
@@ -473,144 +472,3 @@ type VendorKind string
 const (
 	KindVendor VendorKind = "Vendor"
 )
-
-type DashboardElement struct {
-	AnythingArray  []interface{}
-	Bool           *bool
-	DashboardClass *DashboardClass
-	Double         *float64
-	Integer        *int64
-	String         *string
-}
-
-func (x *DashboardElement) UnmarshalJSON(data []byte) error {
-	x.AnythingArray = nil
-	x.DashboardClass = nil
-	var c DashboardClass
-	object, err := unmarshalUnion(data, &x.Integer, &x.Double, &x.Bool, &x.String, true, &x.AnythingArray, true, &c, false, nil, false, nil, true)
-	if err != nil {
-		return err
-	}
-	if object {
-		x.DashboardClass = &c
-	}
-	return nil
-}
-
-func (x *DashboardElement) MarshalJSON() ([]byte, error) {
-	return marshalUnion(x.Integer, x.Double, x.Bool, x.String, x.AnythingArray != nil, x.AnythingArray, x.DashboardClass != nil, x.DashboardClass, false, nil, false, nil, true)
-}
-
-func unmarshalUnion(data []byte, pi **int64, pf **float64, pb **bool, ps **string, haveArray bool, pa interface{}, haveObject bool, pc interface{}, haveMap bool, pm interface{}, haveEnum bool, pe interface{}, nullable bool) (bool, error) {
-	if pi != nil {
-		*pi = nil
-	}
-	if pf != nil {
-		*pf = nil
-	}
-	if pb != nil {
-		*pb = nil
-	}
-	if ps != nil {
-		*ps = nil
-	}
-
-	dec := json.NewDecoder(bytes.NewReader(data))
-	dec.UseNumber()
-	tok, err := dec.Token()
-	if err != nil {
-		return false, err
-	}
-
-	switch v := tok.(type) {
-	case json.Number:
-		if pi != nil {
-			i, err := v.Int64()
-			if err == nil {
-				*pi = &i
-				return false, nil
-			}
-		}
-		if pf != nil {
-			f, err := v.Float64()
-			if err == nil {
-				*pf = &f
-				return false, nil
-			}
-			return false, errors.New("Unparsable number")
-		}
-		return false, errors.New("Union does not contain number")
-	case float64:
-		return false, errors.New("Decoder should not return float64")
-	case bool:
-		if pb != nil {
-			*pb = &v
-			return false, nil
-		}
-		return false, errors.New("Union does not contain bool")
-	case string:
-		if haveEnum {
-			return false, json.Unmarshal(data, pe)
-		}
-		if ps != nil {
-			*ps = &v
-			return false, nil
-		}
-		return false, errors.New("Union does not contain string")
-	case nil:
-		if nullable {
-			return false, nil
-		}
-		return false, errors.New("Union does not contain null")
-	case json.Delim:
-		if v == '{' {
-			if haveObject {
-				return true, json.Unmarshal(data, pc)
-			}
-			if haveMap {
-				return false, json.Unmarshal(data, pm)
-			}
-			return false, errors.New("Union does not contain object")
-		}
-		if v == '[' {
-			if haveArray {
-				return false, json.Unmarshal(data, pa)
-			}
-			return false, errors.New("Union does not contain array")
-		}
-		return false, errors.New("Cannot handle delimiter")
-	}
-	return false, errors.New("Cannot unmarshal union")
-
-}
-
-func marshalUnion(pi *int64, pf *float64, pb *bool, ps *string, haveArray bool, pa interface{}, haveObject bool, pc interface{}, haveMap bool, pm interface{}, haveEnum bool, pe interface{}, nullable bool) ([]byte, error) {
-	if pi != nil {
-		return json.Marshal(*pi)
-	}
-	if pf != nil {
-		return json.Marshal(*pf)
-	}
-	if pb != nil {
-		return json.Marshal(*pb)
-	}
-	if ps != nil {
-		return json.Marshal(*ps)
-	}
-	if haveArray {
-		return json.Marshal(pa)
-	}
-	if haveObject {
-		return json.Marshal(pc)
-	}
-	if haveMap {
-		return json.Marshal(pm)
-	}
-	if haveEnum {
-		return json.Marshal(pe)
-	}
-	if nullable {
-		return json.Marshal(nil)
-	}
-	return nil, errors.New("Union must not be null")
-}
