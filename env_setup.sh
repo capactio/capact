cbe8a40434c655da958763ef442f3299a6a7d23a
marcin.rzucidlo <marcin.rzucidlo@yupo.pl>
2020-10-19 13:38:33 +0200

    build paralelly

--- env_setup.sh
+++ env_setup.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
 echo "GO_VERSION=^1.15.2" >>$GITHUB_ENV
 echo "PROJECT_ID=projectvoltron" >>$GITHUB_ENV
-echo "DOCKER_TAG=$(echo ${GITHUB_SHA:0:7})" >>$GITHUB_ENV
+#echo "DOCKER_TAG=$(echo ${GITHUB_SHA:0:7})" >>$GITHUB_ENV
 #echo "PR_NUMBER=${{ github.event.number }}" >>$GITHUB_ENV
 echo "APPS=gateway k8s-engine och" >>$GITHUB_ENV
+
+PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
+[ -z "${PR_NUMBER}"] && echo "DOCKER_TAG=$(echo ${GITHUB_SHA:0:7})" >>$GITHUB_ENV || echo "DOCKER_TAG=PR-$(echo ${PR_NUMBER})" >>$GITHUB_ENV
