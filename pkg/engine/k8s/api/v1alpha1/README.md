8a23936f8ba42ff782cda3afd23982b0bceb7b0d
Pawel Kosiec <pawel@structsure.co>
2020-10-21 11:53:45 +0200

    Prepare initial structs for Action CR

---
+++ pkg/engine/k8s/api/v1alpha1/README.md
@@ -0,0 +1,14 @@
+<!-- TODO: Move this document -->
+
+# Action CRD
+
+The following document lists all decisions regarding Action Custom Resource Definition.
+
+- Currently there is only namespace-scoped Action resource. In future we may introduce cluster-wide ClusterAction.
+- Following best practices, user provided fields are stored in `Action.spec`, and controller-provided data in `Action.status`.
+
+## Open questions
+<!-- TODO: Remove this section -->
+
+- Can user specify where the output should be placed? If yes, it should be copied to Action.spec as well
+
--- pkg/engine/k8s/api/v1alpha1/action_types.go
+++ pkg/engine/k8s/api/v1alpha1/action_types.go
@@ -1,28 +1,100 @@
 package v1alpha1
 
 import (
+	"encoding/json"
+	"k8s.io/api/authentication/v1beta1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+// TODO: kubebuilder printcolumn
+// TODO: add comments to every field
+// TODO: add validation
+// TODO: conditions?
 
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 // Important: Run "make gen-k8s-resources" to regenerate code after modifying this file
 
-// ActionSpec defines the desired state of Action
+// ActionSpec describes user intention to resolve & execute a given Interface or Implementation.
 type ActionSpec struct {
-	// Foo is an example field of Action.
-	Foo string `json:"foo,omitempty"`
+
+	// Full path for the Implementation or Interface
+	Action ActionPath `json:"action,omitempty"`
+
+	// InputRef contains reference to resource with Action input.
+	InputRef  *ActionIORef `json:"inputRef,omitempty"`
+
+	AdvancedRendering AdvancedRenderingSpec `json:"advancedRendering,omitempty"`
+
+	RenderedActionOverride *json.RawMessage
+
+	Run bool
 }
 
-// ActionStatus defines the observed state of Action
+type AdvancedRenderingSpec struct {
+	Enabled bool `json:"advancedRenderingEnabled,omitempty"`
+	RenderingIteration RenderingIterationSpec
+}
+
+type RenderingIterationSpec struct {
+	InputArtifacts *[]InputArtifact `json:"artifactInput,omitempty"`
+}
+
+type InputArtifact struct {
+	Alias string
+	TypePath NodePath
+	TypeInstanceID *string
+}
+
+// ActionPath defines full path for the Implementation or Interface to run.
+type ActionPath string
+
+type ActionIORef struct {
+	SecretRef *v1.LocalObjectReference `json:"secretRef,omitempty"`
+}
+
+// ActionStatus defines the observed state of Action.
 type ActionStatus struct {
-	Condition ActionCondition
-	Message *string
+	Condition ActionCondition `json:"condition"`
 
+	Message *string `json:"message,omitempty"`
 
+	BuiltinRunner BuiltinRunnerStatus `json:"builtInRunner,omitempty"`
 
-	// Foo is an example field of Action.
-	Foo string `json:"foo,omitempty"`
+	// OutputRef contains reference to resource with Action output.
+	OutputRef *ActionIORef `json:"outputRef,omitempty"`
+
+	RenderedAction *json.RawMessage
+
+	AdvancedRendering *AdvancedRenderingStatus `json:"renderingAdvancedMode,omitempty"`
+
+	CreatedBy   *v1beta1.UserInfo `json:"createdBy,omitempty"`
+	RunBy       *v1beta1.UserInfo `json:"runBy,omitempty"`
+	CancelledBy *v1beta1.UserInfo `json:"cancelledBy,omitempty"`
 }
+
+type AdvancedRenderingStatus struct {
+	RenderingIteration *RenderingIterationStatus
+}
+
+type RenderingIterationStatus struct {
+	InputArtifactsToProvide *[]InputArtifact
+}
+
+type InputArtifactToProvide struct {
+	Alias string
+	TypePath NodePath
+}
+
+type BuiltinRunnerStatus struct {
+
+	Interface NodePath `json:"interface,omitempty"`
+
+	// StatusRef contains reference to resource with built-in Runner status data.
+	Status *json.RawMessage `json:"status,omitempty"`
+}
+
+type NodePath string
 
 type ActionCondition string
 
@@ -39,7 +111,7 @@ const (
 )
 
 // +kubebuilder:object:root=true
-
+// +kubebuilder:subresource:status
 // Action is the Schema for the actions API
 type Action struct {
 	metav1.TypeMeta   `json:",inline"`
