c9f6d70195777095731d8f3f798b43fdb141bfdf
marcin.rzucidlo <marcin.rzucidlo@yupo.pl>
2020-10-21 13:24:42 +0200

    cleanup

--- terraform/google_compute_network.tf
+++
@@ -1,7 +0,0 @@
-resource "google_compute_network" "gcn_vpc" {
-  name   = var.google_compute_network_name
-  project = var.project
-  auto_create_subnetworks = false
-}
-
-#TO DO PEERINGI i PRIVATE SERVICE CONNECTION
