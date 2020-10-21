1f06026d693adbc8412ed42b6c36bb5199176fda
marcin.rzucidlo <marcin.rzucidlo@yupo.pl>
2020-10-21 08:18:03 +0200

    matrix

--- env_setup.sh
+++ env_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "GO_VERSION=^1.15.2" >>$GITHUB_ENV
 echo "PROJECT_ID=projectvoltron" >>$GITHUB_ENV
-echo "APPS=gateway k8s-engine och" >>$GITHUB_ENV
+echo "APPS=gateway, k8s-engine, och" >>$GITHUB_ENV
 
 if [ "${GITHUB_EVENT_NAME}" = "push" ]
 then
