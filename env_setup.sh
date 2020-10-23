eabefa4eae22d4e79dd31ac0d85a66a6a63f3391
marcin.rzucidlo <marcin.rzucidlo@yupo.pl>
2020-10-23 16:02:52 +0200

    PR comments applied

--- env_setup.sh
+++ env_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "SKIP_DEPS_INSTALLATION=false" >>$GITHUB_ENV
 echo "GO_VERSION=^1.15.2" >>$GITHUB_ENV
 echo "PROJECT_ID=projectvoltron" >>$GITHUB_ENV
 echo "LINT_TIMEOUT=2m" >>$GITHUB_ENV
@@ -13,12 +14,8 @@ else
   echo "DOCKER_PUSH_REPOSITORY=gcr.io/projectvoltron/pr" >>$GITHUB_ENV
 fi
 
-echo "TF_VAR_cluster_name=voltron-dev1" >>$GITHUB_ENV
-echo "TF_VAR_location=europe-west3" >>$GITHUB_ENV
-echo "" >>$GITHUB_ENV
-
-echo APPS="name=matrix::{\"include\":[{\"APP\":\"gateway\"},{\"APP\":\"k8s-engine\"},{\"APP\":\"och\"}]}" >>$GITHUB_ENV
-echo TESTS="name=matrix::{\"include\":[{\"TEST\":\"e2e\"}]}" >>$GITHUB_ENV
-echo INFRAS="name=matrix::{\"include\":[{\"INFRA\":\"json-go-gen\"}]}" >>$GITHUB_ENV
+echo APPS='name=matrix::{"include":[{"APP":"gateway"},{"APP":"k8s-engine"},{"APP":"och"}]}' >>$GITHUB_ENV
+echo TESTS='name=matrix::{"include":[{"TEST":"e2e"}]}' >>$GITHUB_ENV
+echo INFRAS='name=matrix::{"include":[{"INFRA":"json-go-gen"}]}' >>$GITHUB_ENV
 
 
