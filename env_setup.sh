5929c76673acd8b4541ed604da172d133aa51fb1
marcin.rzucidlo <marcin.rzucidlo@yupo.pl>
2020-10-26 16:50:13 +0100

    cluster

--- env_setup.sh
+++ env_setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "SKIP_DEPS_INSTALLATION=false" >>$GITHUB_ENV
 echo "GO_VERSION=^1.15.2" >>$GITHUB_ENV
 echo "PROJECT_ID=projectvoltron" >>$GITHUB_ENV
 echo "LINT_TIMEOUT=2m" >>$GITHUB_ENV
@@ -14,8 +13,23 @@ else
   echo "DOCKER_PUSH_REPOSITORY=gcr.io/projectvoltron/pr" >>$GITHUB_ENV
 fi
 
-echo APPS='name=matrix::{"include":[{"APP":"gateway"},{"APP":"k8s-engine"},{"APP":"och"}]}' >>$GITHUB_ENV
-echo TESTS='name=matrix::{"include":[{"TEST":"e2e"}]}' >>$GITHUB_ENV
-echo INFRAS='name=matrix::{"include":[{"INFRA":"json-go-gen"}]}' >>$GITHUB_ENV
+echo APPS="name=matrix::{\"include\":[{\"APP\":\"gateway\"},{\"APP\":\"k8s-engine\"},{\"APP\":\"och\"}]}" >>$GITHUB_ENV
+echo TESTS="name=matrix::{\"include\":[{\"TEST\":\"e2e\"}]}" >>$GITHUB_ENV
+echo INFRAS="name=matrix::{\"include\":[{\"INFRA\":\"json-go-gen\"}]}" >>$GITHUB_ENV
 
-
+#Create & upgrade cluster related
+#IMAGE_TAG is the TAG which you assign when you recreate the cluster
+export NAME=dev3
+export REGION=europe-north1
+echo "IMAGE_TAG=adea064" >>$GITHUB_ENV
+echo "TF_VAR_region=${REGION}" >>$GITHUB_ENV
+echo "TF_VAR_cluster_name=voltron-${NAME}" >>$GITHUB_ENV
+echo "TF_VAR_google_compute_network_name=vpc-network-${NAME}" >>$GITHUB_ENV
+echo "TF_VAR_google_compute_subnetwork_name=subnetwork-${NAME}" >>$GITHUB_ENV
+echo "TF_VAR_node_pool_name=node-pool-${NAME}" >>$GITHUB_ENV
+echo "TF_VAR_google_compute_subnetwork_secondary_ip_range_name1=gke-pods-${NAME}" >>$GITHUB_ENV
+echo "TF_VAR_google_compute_subnetwork_secondary_ip_range_name2=gke-services-${NAME}" >>$GITHUB_ENV
+echo "GET_IP_SERVICE=icanhazip.com" >>$GITHUB_ENV
+echo "NAMESPACE=voltron" >>$GITHUB_ENV
+echo "SERVICES=gateway engine och-public och-local" >>$GITHUB_ENV
+echo "HELM_TEST_TIMEOUT=10m"  >>$GITHUB_ENV
