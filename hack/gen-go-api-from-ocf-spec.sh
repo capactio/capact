9b2a7246be162b25d0f5793c88cecb47190494b1
Pawel Kosiec <pawel@structsure.co>
2020-10-27 12:28:58 +0100

    Bump Kubebuilder version

--- hack/gen-go-api-from-ocf-spec.sh
+++ hack/gen-go-api-from-ocf-spec.sh
@@ -18,7 +18,7 @@ source "${CURRENT_DIR}/lib/utilities.sh" || { echo 'Cannot load CI utilities.'; 
 # shellcheck source=./hack/lib/const.sh
 source "${CURRENT_DIR}/lib/const.sh" || { echo 'Cannot load constant values.' exit 1; }
 
-OCF_VERSION="${OCF_VERSION:-DEFAULT_OCF_VERSION}"
+OCF_VERSION="${OCF_VERSION:-$DEFAULT_OCF_VERSION}"
 
 REPORT_FILE_DIR="${REPO_ROOT_DIR}/tmp"
 REPORT_FILENAME="${REPORT_FILE_DIR}/gen_go_api_issues.txt"
@@ -29,10 +29,10 @@ check_for_unknown_issues() {
 
   if ! diff -u "${REPORT_FILENAME}" "${KNOWN_VIOLATION_FILENAME}"; then
     echo "Error:
-    API rules check failed. Reported violations \"${REPORT_FILENAME}\" differ from known violations \"${KNOWN_VIOLATION_FILENAME}\":"
+    API rules check failed. Reported violations \"${REPORT_FILENAME}\" differ from known violations \"${KNOWN_VIOLATION_FILENAME}\"."
     
     diff -u "${REPORT_FILENAME}" "${KNOWN_VIOLATION_FILENAME}"
-    
+
     echo "Please fix API source file if new violation is detected, or update known violations \"${KNOWN_VIOLATION_FILENAME}\" if existing violation is being fixed."
     exit 1
   fi
