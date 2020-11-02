341f7e3506bcdddf9aed72ded66060fd49ba7127
marcin.rzucidlo <marcin.rzucidlo@yupo.pl>
2020-11-02 10:29:50 +0100

    env setup

--- env_setup.sh
+++ env_setup.sh
@@ -22,11 +22,21 @@ fi
 APPS="gateway k8s-engine och"
 APPS=$(echo 'name=matrix::{"include":['; for APP in ${APPS}; do echo {\"APP\":\"${APP}\"},; done; echo ']}' |tr -d "\n")
 export APPS=$(echo ${APPS} |sed 's/}, ]/} ]/g' )
-
 echo "APPS=${APPS}" >>$GITHUB_ENV
+
+TESTS="e2e"
+TESTS=$(echo 'name=matrix::{"include":['; for TEST in ${TESTS}; do echo {\"TEST\":\"${TEST}\"},; done; echo ']}' |tr -d "\n")
+export TESTS=$(echo ${TESTS} |sed 's/}, ]/} ]/g' )
+echo "TESTS=${TESTS}" >>$GITHUB_ENV
+
+INFRAS="json-go-gen"
+INFRAS=$(echo 'name=matrix::{"include":['; for INFRA in ${INFRAS}; do echo {\"INFRA\":\"${INFRA}\"},; done; echo ']}' |tr -d "\n")
+export INFRAS=$(echo ${INFRAS} |sed 's/}, ]/} ]/g' )
+echo "INFRAS=${INFRAS}" >>$GITHUB_ENV
+
 #echo APPS='name=matrix::{"include":[{"APP":"gateway"},{"APP":"k8s-engine"},{"APP":"och"}]}' >>$GITHUB_ENV
-echo TESTS="name=matrix::{\"include\":[{\"TEST\":\"e2e\"}]}" >>$GITHUB_ENV
-echo INFRAS="name=matrix::{\"include\":[{\"INFRA\":\"json-go-gen\"}]}" >>$GITHUB_ENV
+#echo TESTS="name=matrix::{\"include\":[{\"TEST\":\"e2e\"}]}" >>$GITHUB_ENV
+#echo INFRAS="name=matrix::{\"include\":[{\"INFRA\":\"json-go-gen\"}]}" >>$GITHUB_ENV
 
 #Create & upgrade cluster related
 #IMAGE_TAG is the TAG which you assign when you recreate the cluster
