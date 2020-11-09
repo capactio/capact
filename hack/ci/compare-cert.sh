ca77f48a12222fc79e04e00681c56588bab3dac5
marcin.rzucidlo <marcin.rzucidlo@yupo.pl>
2020-11-09 15:30:16 +0100

    final version

--- hack/ci/compare-cert.sh
+++ hack/ci/compare-cert.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-CREATED=$(gsutil ls -la gs://projectvoltron_le |sort -k2 -r |grep -v ^TOTAL |head -n 1 |awk -F"#" '{ print substr($2,1,10) }')
+CREATED=$(gsutil ls -la gs://${BUCKET} |sort -k2 -r |grep -v ^TOTAL |head -n 1 |awk -F"#" '{ print substr($2,1,10) }')
+if [ -z "${CREATED}" ] 
+then 
+  printf "\n***Certs not existing in the bucket. New one needed to be generated***\n";
+  exit
+fi
+
 if (( $(echo $((($(date +%s) - ${CREATED} ) / 86400))) > ${CERT_MAX_AGE} )); then
-echo -e "\n- New cert needs to be generated -"
+   echo -e "\n- New cert needs to be generated -"
 else
-echo -e "\n- Cert still valid. Restoring -"
-gsutil cp $(gsutil ls gs://projectvoltron_le  |sort -k2 -r |grep -v ^TOTAL |head -n 1) .
-kubectl create ns ${NAMESPACE} || true
-kubectl apply -f secret-*.yaml --namespace ${NAMESPACE}
-export CERT_RESTORE=1
+  echo -e "\n- Cert still valid. Restoring -"
+  gsutil cp $(gsutil ls gs://${BUCKET}  |sort -k2 -r |grep -v ^TOTAL |head -n 1) .
+  kubectl create ns ${NAMESPACE} || true
+  kubectl apply -f secret-*.yaml --namespace ${NAMESPACE}
+  export CERT_RESTORE=1
 fi
