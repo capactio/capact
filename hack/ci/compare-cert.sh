e878268935901b5bc9267bc286f382a47ca33635
marcin.rzucidlo <marcin.rzucidlo@yupo.pl>
2020-11-16 09:19:06 +0100

    pipeline helm

--- hack/ci/compare-cert.sh
+++ hack/ci/compare-cert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-CREATED=$(gsutil ls -la gs://${BUCKET} |sort -k2 -r |grep -v ^TOTAL |head -n 1 |awk -F"#" '{ print substr($2,1,10) }')
+export CREATED=$(gsutil ls -la gs://${BUCKET} |sort -k2 -r |grep -v ^TOTAL |head -n 1 |awk -F"#" '{ print substr($2,1,10) }')
+echo "CREATED=${CREATED}" >> $GITHUB_ENV
+
 if [ -z "${CREATED}" ] 
 then 
   printf "\n***Certs not existing in the bucket. New one needed to be generated***\n";
