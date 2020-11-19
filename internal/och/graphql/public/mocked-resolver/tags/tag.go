b5c02aaeddd047465471819eabde014fc050a29d
Łukasz Oleś <lukasz@structsure.co>
2020-11-19 10:41:37 +0100

    Mock Type

--- internal/och/graphql/public/mocked-resolver/tags/tag.go
+++ internal/och/graphql/public/mocked-resolver/tags/tag.go
@@ -2,7 +2,9 @@ package tags
 
 import (
 	"context"
+	"fmt"
 
+	mockedresolver "projectvoltron.dev/voltron/internal/och/graphql/public/mocked-resolver"
 	gqlpublicapi "projectvoltron.dev/voltron/pkg/och/api/graphql/public"
 )
 
@@ -13,21 +15,30 @@ func NewResolver() *TagResolver {
 }
 
 func (r *TagResolver) Tags(ctx context.Context, filter *gqlpublicapi.TagFilter) ([]*gqlpublicapi.Tag, error) {
-	return []*gqlpublicapi.Tag{dummyTag("kubernetes"), dummyTag("cloudFoundry")}, nil
+	tag, err := mockedresolver.MockedTag()
+	if err != nil {
+		return []*gqlpublicapi.Tag{}, err
+	}
+	return []*gqlpublicapi.Tag{tag}, nil
 }
 
 func (r *TagResolver) Tag(ctx context.Context, path string) (*gqlpublicapi.Tag, error) {
-	return dummyTag("kubernetes"), nil
+	tag, err := mockedresolver.MockedTag()
+	if err != nil {
+		return &gqlpublicapi.Tag{}, err
+	}
+	return tag, nil
 }
 
 func (r *TagResolver) Revision(ctx context.Context, obj *gqlpublicapi.Tag, revision string) (*gqlpublicapi.TagRevision, error) {
-	return &gqlpublicapi.TagRevision{}, nil
-}
-
-func dummyTag(name string) *gqlpublicapi.Tag {
-	return &gqlpublicapi.Tag{
-		Name:   name,
-		Prefix: "cap.tag.platform",
-		Path:   "cap.tag.platform." + name,
+	tag, err := mockedresolver.MockedTag()
+	if err != nil {
+		return &gqlpublicapi.TagRevision{}, err
 	}
+	for _, r := range tag.Revisions {
+		if r.Revision == revision {
+			return r, nil
+		}
+	}
+	return &gqlpublicapi.TagRevision{}, fmt.Errorf("No tag with revision %s", revision)
 }
