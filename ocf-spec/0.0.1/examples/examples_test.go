672a989c59424ec7a9d5f7db91df42a7e61c7c68
Damian Czaja <trojan295@gmail.com>
2020-11-19 14:21:19 +0100

    fix tests

--- ocf-spec/0.0.1/examples/examples_test.go
+++ ocf-spec/0.0.1/examples/examples_test.go
@@ -3,7 +3,6 @@
 package examples
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,28 +10,7 @@ import (
 )
 
 func TestManifestsValid(t *testing.T) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-	// Load the common schemas. Currently, the https $ref is not working as we didn't publish the spec yet.
-	sl := gojsonschema.NewSchemaLoader()
-
-	schemaRefPaths := []string{
-		"../schema/common/json-schema-type.json",
-		"../schema/common/type-ref.json",
-		"../schema/common/input-type-instances.json",
-		"../schema/common/output-type-instances.json",
-		"../schema/common/metadata.json",
-		"../schema/common/metadata-tags.json",
-	}
-	err := loadCommonSchemas(sl, schemaRefPaths)
-	require.NoError(t, err, "while loading common schemas")
-=======
-	validator, err := ocftool.NewFilesystemManifestValidator("../..")
-	require.NoError(t, err, "while creating validator instance")
->>>>>>> ef5e95c... working cli
-=======
 	validator := ocftool.NewFilesystemManifestValidator("../..")
->>>>>>> f4cdb86... fix tests
 
 	tests := map[string]struct {
 		manifestPath string
@@ -66,16 +44,12 @@ func TestManifestsValid(t *testing.T) {
 	for tn, tc := range tests {
 		t.Run(tn, func(t *testing.T) {
 			// given
-			fp, err := os.Open(tc.manifestPath)
-			require.NoError(t, err, "while reading manifest test file")
-			defer fp.Close()
 
 			// when
-			result, err := validator.ValidateYaml(fp)
+			result := validator.ValidateFile(tc.manifestPath)
 
 			// then
-			require.NoError(t, err, "while validating object against JSON Schema")
-			require.True(t, result.Valid(), "is not valid")
+			require.True(t, result.Valid(), "is not valid, errors: %v", result.Errors)
 		})
 	}
 }
