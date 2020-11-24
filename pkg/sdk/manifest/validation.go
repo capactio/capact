110cc5e63a726004f67de982794b9533a087ff9f
Damian Czaja <trojan295@gmail.com>
2020-11-24 11:03:47 +0100

    from general to specific

--- pkg/sdk/manifest/validation.go
+++ pkg/sdk/manifest/validation.go
@@ -10,8 +10,16 @@ import (
 	"github.com/xeipuuv/gojsonschema"
 )
 
+type Validator interface {
+	ValidateFile(filepath string) (ValidationResult, error)
+}
+
 type ValidationResult struct {
 	Errors []error
+}
+
+func (r *ValidationResult) Valid() bool {
+	return len(r.Errors) == 0
 }
 
 func newValidationResult(errors ...error) ValidationResult {
@@ -20,12 +28,9 @@ func newValidationResult(errors ...error) ValidationResult {
 	}
 }
 
-func (r *ValidationResult) Valid() bool {
-	return len(r.Errors) == 0
-}
-
-type Validator interface {
-	ValidateFile(filepath string) (ValidationResult, error)
+type FilesystemManifestValidator struct {
+	schemaRootPath string
+	cachedSchemas  map[ocfVersion]*loadedOCFSchema
 }
 
 type ocfVersion string
@@ -37,16 +42,52 @@ type loadedOCFSchema struct {
 	kind   map[kind]*gojsonschema.Schema
 }
 
-type FilesystemManifestValidator struct {
-	schemaRootPath string
-	cachedSchemas  map[ocfVersion]*loadedOCFSchema
-}
-
 func NewFilesystemValidator(schemaRootPath string) Validator {
 	return &FilesystemManifestValidator{
 		schemaRootPath: schemaRootPath,
 		cachedSchemas:  map[ocfVersion]*loadedOCFSchema{},
 	}
+}
+
+func (v *FilesystemManifestValidator) ValidateFile(filepath string) (ValidationResult, error) {
+	data, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return newValidationResult(), err
+	}
+
+	return v.validateYamlBytes(data)
+}
+
+func (v *FilesystemManifestValidator) validateYamlBytes(yamlBytes []byte) (ValidationResult, error) {
+	metadata, err := getManifestMetadata(yamlBytes)
+	if err != nil {
+		return newValidationResult(errors.Wrap(err, "failed to read manifest metadata")), err
+	}
+
+	schema, err := v.getManifestSchema(metadata)
+	if err != nil {
+		return newValidationResult(), errors.Wrap(err, "failed to get JSON schema")
+	}
+
+	jsonBytes, err := yaml.YAMLToJSON(yamlBytes)
+	if err != nil {
+		return newValidationResult(errors.Wrap(err, "cannot convert YAML manifest to JSON")), err
+	}
+
+	manifestLoader := gojsonschema.NewBytesLoader(jsonBytes)
+
+	jsonschemaResult, err := schema.Validate(manifestLoader)
+	if err != nil {
+		return newValidationResult(errors.Wrap(err, "error occurred during JSON schema validation")), err
+	}
+
+	result := newValidationResult()
+
+	for _, err := range jsonschemaResult.Errors() {
+		result.Errors = append(result.Errors, fmt.Errorf("%v", err.String()))
+	}
+
+	return result, err
 }
 
 type manifestMetadata struct {
@@ -61,31 +102,6 @@ func getManifestMetadata(yamlBytes []byte) (manifestMetadata, error) {
 		return mm, err
 	}
 	return mm, nil
-}
-
-func (v *FilesystemManifestValidator) getCommonSchemaLoader(ocfVersion ocfVersion) (*gojsonschema.SchemaLoader, error) {
-	commonDir := fmt.Sprintf("%s/%s/schema/common", v.schemaRootPath, ocfVersion)
-
-	sl := gojsonschema.NewSchemaLoader()
-	files, err := ioutil.ReadDir(commonDir)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to list common schemas directory")
-	}
-
-	for _, file := range files {
-		path := fmt.Sprintf("file://%s/%s", commonDir, file.Name())
-		if err := sl.AddSchemas(gojsonschema.NewReferenceLoader(path)); err != nil {
-			return nil, errors.Wrapf(err, "cannot load common schema %s", path)
-		}
-	}
-
-	return sl, nil
-}
-
-func (v *FilesystemManifestValidator) getRootSchemaJSONLoader(metadata manifestMetadata) gojsonschema.JSONLoader {
-	filename := strcase.ToKebab(string(metadata.Kind))
-	path := fmt.Sprintf("file://%s/%s/schema/%s.json", v.schemaRootPath, metadata.OCFVersion, filename)
-	return gojsonschema.NewReferenceLoader(path)
 }
 
 func (v *FilesystemManifestValidator) getManifestSchema(metadata manifestMetadata) (*gojsonschema.Schema, error) {
@@ -124,43 +140,27 @@ func (v *FilesystemManifestValidator) getManifestSchema(metadata manifestMetadat
 	return schema, nil
 }
 
-func (v *FilesystemManifestValidator) validateYamlBytes(yamlBytes []byte) (ValidationResult, error) {
-	metadata, err := getManifestMetadata(yamlBytes)
-	if err != nil {
-		return newValidationResult(errors.Wrap(err, "failed to read manifest metadata")), err
-	}
-
-	schema, err := v.getManifestSchema(metadata)
-	if err != nil {
-		return newValidationResult(), errors.Wrap(err, "failed to get JSON schema")
-	}
-
-	jsonBytes, err := yaml.YAMLToJSON(yamlBytes)
-	if err != nil {
-		return newValidationResult(errors.Wrap(err, "cannot convert YAML manifest to JSON")), err
-	}
-
-	manifestLoader := gojsonschema.NewBytesLoader(jsonBytes)
-
-	jsonschemaResult, err := schema.Validate(manifestLoader)
-	if err != nil {
-		return newValidationResult(errors.Wrap(err, "error occurred during JSON schema validation")), err
-	}
-
-	result := newValidationResult()
-
-	for _, err := range jsonschemaResult.Errors() {
-		result.Errors = append(result.Errors, fmt.Errorf("%v", err.String()))
-	}
-
-	return result, err
+func (v *FilesystemManifestValidator) getRootSchemaJSONLoader(metadata manifestMetadata) gojsonschema.JSONLoader {
+	filename := strcase.ToKebab(string(metadata.Kind))
+	path := fmt.Sprintf("file://%s/%s/schema/%s.json", v.schemaRootPath, metadata.OCFVersion, filename)
+	return gojsonschema.NewReferenceLoader(path)
 }
 
-func (v *FilesystemManifestValidator) ValidateFile(filepath string) (ValidationResult, error) {
-	data, err := ioutil.ReadFile(filepath)
+func (v *FilesystemManifestValidator) getCommonSchemaLoader(ocfVersion ocfVersion) (*gojsonschema.SchemaLoader, error) {
+	commonDir := fmt.Sprintf("%s/%s/schema/common", v.schemaRootPath, ocfVersion)
+
+	sl := gojsonschema.NewSchemaLoader()
+	files, err := ioutil.ReadDir(commonDir)
 	if err != nil {
-		return newValidationResult(), err
+		return nil, errors.Wrap(err, "failed to list common schemas directory")
 	}
 
-	return v.validateYamlBytes(data)
+	for _, file := range files {
+		path := fmt.Sprintf("file://%s/%s", commonDir, file.Name())
+		if err := sl.AddSchemas(gojsonschema.NewReferenceLoader(path)); err != nil {
+			return nil, errors.Wrapf(err, "cannot load common schema %s", path)
+		}
+	}
+
+	return sl, nil
 }
