304173933450b9451b783dcf92394c1fe41d55fd
Damian Czaja <trojan295@gmail.com>
2021-01-19 11:16:25 +0100

    fix issue with getting name of prefix without path

--- och-js/src/schema/public.ts
+++ och-js/src/schema/public.ts
@@ -1,49 +1,18 @@
 import { readFileSync } from 'fs';
-import { makeAugmentedSchema } from 'neo4j-graphql-js';
+import { makeAugmentedSchema, neo4jgraphql } from 'neo4j-graphql-js';
 import { IResolvers } from 'graphql-tools';
+import { GraphQLResolveInfo } from 'graphql/type';
 
 const typeDefs = readFileSync('./graphql/public.graphql', 'utf-8');
 
 const nameResolver = (object: { path: string }) => object.path.split('.').pop();
 
-const prefixResolver = (object: { path: string }) => {
+const prefixResolver = (object: { path: string }, params, context, resolveInfo) => {
   const parts = object.path.split('.');
   return parts.slice(0, parts.length - 1).join('.');
 };
 
 const resolvers: IResolvers = {
-  RepoMetadata: {
-    name: nameResolver,
-    prefix: prefixResolver,
-  },
-  Interface: {
-    name: nameResolver,
-    prefix: prefixResolver,
-  },
-  Type: {
-    name: nameResolver,
-    prefix: prefixResolver,
-  },
-  Implementation: {
-    name: nameResolver,
-    prefix: prefixResolver,
-  },
-  Attribute: {
-    name: nameResolver,
-    prefix: prefixResolver,
-  },
-  GenericMetadata: {
-    name: nameResolver,
-    prefix: prefixResolver,
-  },
-  ImplementationMetadata: {
-    name: nameResolver,
-    prefix: prefixResolver,
-  },
-  TypeMetadata: {
-    name: nameResolver,
-    prefix: prefixResolver,
-  },
 };
 
 const schema = makeAugmentedSchema({
