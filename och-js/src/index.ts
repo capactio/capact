d7efbb9bb8f831e81e6ccaa727edeed37758c6e3
Damian Czaja <trojan295@gmail.com>
2021-01-20 11:54:48 +0100

    test husky

--- och-js/src/index.ts
+++ och-js/src/index.ts
@@ -1,13 +1,13 @@
-import { ApolloServer } from 'apollo-server-express';
-import * as express from 'express';
-import neo4j, { Driver } from 'neo4j-driver';
-import { createTerminus } from '@godaddy/terminus';
-import * as http from 'http';
-import { GraphQLSchema } from 'graphql';
+import { ApolloServer } from "apollo-server-express";
+import * as express from "express";
+import neo4j, { Driver } from "neo4j-driver";
+import { createTerminus } from "@godaddy/terminus"
+import * as http from "http";
+import { GraphQLSchema } from "graphql";
 
-import { getSchemaForMode, assertSchemaOnDatabase } from './schema';
-import config from './config';
-import logger from './logger';
+import { getSchemaForMode, assertSchemaOnDatabase } from "./schema";
+import config from "./config";
+import logger from "./logger";
 
 function setupHttpServer(schema: GraphQLSchema, driver: Driver): http.Server {
   const app = express();
@@ -19,17 +19,17 @@ function setupHttpServer(schema: GraphQLSchema, driver: Driver): http.Server {
 
   const healthCheck = () => Promise.resolve();
   createTerminus(server, {
-    healthChecks: { '/healthz': healthCheck },
+    healthChecks: { "/healthz": healthCheck },
   });
 
   return server;
 }
 
-logger.info(`Using Neo4j database at ${config.neo4j.endpoint}`);
+logger.info("Using Neo4j database", { endpoint: config.neo4j.endpoint });
 
 const driver = neo4j.driver(
   config.neo4j.endpoint,
-  neo4j.auth.basic(config.neo4j.username, config.neo4j.password),
+  neo4j.auth.basic(config.neo4j.username, config.neo4j.password)
 );
 
 const schema = getSchemaForMode(config.ochMode);
@@ -40,8 +40,10 @@ assertSchemaOnDatabase(schema, driver);
 const server = setupHttpServer(schema, driver);
 const { bindPort, bindAddress } = config.graphql;
 
-logger.info(`Starting OCH in ${config.ochMode} mode`);
+logger.info("Starting OCH", { mode: config.ochMode });
 
 server.listen(bindPort, bindAddress, () => {
-  logger.info(`GraphQL API listening on http://${bindAddress}:${bindPort}...`);
+  logger.info("GraphQL API is listening", {
+    endpoint: `http://${bindAddress}:${bindPort}`,
+  });
 });
