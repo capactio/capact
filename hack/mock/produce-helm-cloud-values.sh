b73d87063af30626f674bce1e93b0c25c2390f91
trojan295 <trojan295@gmail.com>
2021-03-18 09:27:48 +0100

    rebase from master, remove hack/mock directory completly

--- hack/mock/produce-helm-cloud-values.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-yq w -i /out/jira-cloud-values.yaml databaseConnection.host "$(yq r input.yaml host)"
-yq w -i /out/jira-cloud-values.yaml databaseConnection.database "$(yq r input.yaml defaultDBName)"
-
-sleep 5
