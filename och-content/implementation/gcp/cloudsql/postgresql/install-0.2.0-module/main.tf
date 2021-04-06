84f206cb55116857aad75c512e0b41bdab6974d2
Pawel Kosiec <pawel@structsure.co>
2021-04-06 08:37:49 +0200

    Split CloudSQL Terraform module to multiple files

--- och-content/implementation/gcp/cloudsql/postgresql/install-0.2.0-module/main.tf
+++ och-content/implementation/gcp/cloudsql/postgresql/install-0.2.0-module/main.tf
@@ -1,51 +1,5 @@
-/*
-variable "project" {
-  type = string
-  description = "Google Cloud project"
-} 
-*/
-
-variable "database_version" {
-  type = string
-  default = "POSTGRES_12"
-  description = "CloudSQL database version"
-}
-
-variable "region" {
-  type = string
-  default = "us-central"
-  description = "Google cloud zone"
-}
-
-variable "tier" {
-  type = string
-  default = "db-f1-micro"
-  description = "CloudSQL instance tier"
-}
-
-variable "user_name" {
-  type = string
-  description = "Database user name"
-}
-
-variable "user_password" {
-  type = string
-  description = "Database user password"
-  sensitive = true
-}
-
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = ">=3.55.0"
-    }
-  }
-}
-
 provider "google" {
 }
-
 
 resource "google_sql_database_instance" "master" {
   database_version = var.database_version
@@ -69,17 +23,4 @@ resource "google_sql_user" "users" {
   name     = var.user_name
   instance = google_sql_database_instance.master.name
   password = var.user_password
-}
-
-output "instance_ip_addr" {
-  value = google_sql_database_instance.master.public_ip_address
-}
-
-output "username" {
-  value = google_sql_user.users.name
-}
-
-output "password" {
-  value = google_sql_user.users.password
-  sensitive = true
 }
