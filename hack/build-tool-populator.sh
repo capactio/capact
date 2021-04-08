e2f71fd73a1fb67ef1b4253cad3c6a4fd1b38427
Paul Pietkiewicz <pawel.pietkiewicz@gmail.com>
2021-04-08 13:37:48 -0400

    Add standalone script for building populator independently

---
+++ hack/build-tool-populator.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# This script build the populator CLI. Runs the generate command to ensure OCF schemas are embedded.
+#
+
+# standard bash error handling
+set -o nounset # treat unset variables as an error and exit immediately.
+set -o errexit # exit immediately when a command fails.
+set -E         # needs to be set if we want the ERR trap
+
+readonly CURRENT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+readonly REPO_ROOT_DIR=$(cd "${CURRENT_DIR}/.." && pwd)
+
+ARCHs="amd64"
+OSes="linux darwin windows"
+
+main () {
+  for ARCH in $ARCHs; do
+    for OS in $OSes; do
+      echo "- Building populator binary [OS: ${OS} ARCH: ${ARCH}]..."
+      binary="bin/populator-$OS-$ARCH"
+
+      GOOS=$OS GOARCH=$ARCH go build -ldflags "-s -w" -o "$binary" "${REPO_ROOT_DIR}/cmd/populator/main.go"
+    done
+  done
+}
+
+main
