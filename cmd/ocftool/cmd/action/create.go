624be5bbb2f435f3f2316aab827598c08ed93dd6
Mateusz Szostok <mateusz@structsure.co>
2021-04-21 14:14:34 +0200

    Make bitbucket manifests production-ready, add cli `action create` enhancement (#245)

--- cmd/ocftool/cmd/action/create.go
+++ cmd/ocftool/cmd/action/create.go
@@ -4,6 +4,8 @@ import (
 	"os"
 
 	"capact.io/capact/internal/ocftool/action"
+
+	"github.com/MakeNowJust/heredoc"
 	"github.com/spf13/cobra"
 )
 
@@ -22,7 +24,14 @@ func NewCreate() *cobra.Command {
 	}
 	flags := cmd.Flags()
 	flags.StringVarP(&opts.Namespace, "namespace", "n", "", "Kubernetes namespace where the Action is to be created")
-	flags.BoolVarP(&opts.DryRun, "dry-run", "", false, "Specifies whether the Action performs server-side test without actually running the Action")
+	flags.StringVar(&opts.ActionName, "name", "", "The Action name. By default, a random name is generated.")
+	flags.StringVar(&opts.ParametersFilePath, "parameters-from-file", "", "The Action input parameters in YAML format")
+	flags.StringVar(&opts.TypeInstancesFilePath, "type-instances-from-file", "", heredoc.Doc(`The Action input TypeInstances in YAML format. Example:
+						typeInstances:
+						  - name: "config"
+						    id: "ABCD-1234-EFGH-4567"`))
+	flags.BoolVarP(&opts.Interactive, "interactive", "i", false, "Toggle interactive prompting in the terminal")
+	flags.BoolVar(&opts.DryRun, "dry-run", false, "Specifies whether the Action performs server-side test without actually running the Action")
 	// TODO: add support for creating an action directly from an implementation
 	return cmd
 }
--- internal/ocftool/action/create.go
+++ internal/ocftool/action/create.go
@@ -3,30 +3,14 @@ package action
 import (
 	"context"
 	"io"
-	"math/rand"
-	"strings"
-	"time"
-
-	"github.com/MakeNowJust/heredoc"
 
 	"capact.io/capact/internal/k8s-engine/graphql/namespace"
 	"capact.io/capact/internal/ocftool/client"
 	"capact.io/capact/internal/ocftool/config"
 	"capact.io/capact/internal/ptr"
 	gqlengine "capact.io/capact/pkg/engine/api/graphql"
-
-	"github.com/AlecAivazis/survey/v2"
-	"github.com/docker/docker/pkg/namesgenerator"
 	"github.com/fatih/color"
-	"sigs.k8s.io/yaml"
 )
-
-type CreateOptions struct {
-	InterfacePath string
-	ActionName    string `survey:"name"`
-	Namespace     string
-	DryRun        bool
-}
 
 type CreateOutput struct {
 	Action    *gqlengine.Action
@@ -34,23 +18,7 @@ type CreateOutput struct {
 }
 
 func Create(ctx context.Context, opts CreateOptions, w io.Writer) (*CreateOutput, error) {
-	rand.Seed(time.Now().UnixNano())
-
-	// must be a DNS-1123 subdomain
-	defaultActionName := strings.Replace(namesgenerator.GetRandomName(0), "_", "-", 1)
-	qs := []*survey.Question{
-		actionNameQuestion(defaultActionName),
-	}
-	if opts.Namespace == "" {
-		qs = append(qs, namespaceQuestion())
-	}
-
-	if err := survey.Ask(qs, &opts); err != nil {
-		return nil, err
-	}
-
-	input, err := askForInputParams()
-	if err != nil {
+	if err := opts.Resolve(); err != nil {
 		return nil, err
 	}
 
@@ -67,7 +35,7 @@ func Create(ctx context.Context, opts CreateOptions, w io.Writer) (*CreateOutput
 	ctxWithNs := namespace.NewContext(ctx, opts.Namespace)
 	act, err := actionCli.CreateAction(ctxWithNs, &gqlengine.ActionDetailsInput{
 		Name:  opts.ActionName,
-		Input: input,
+		Input: opts.ActionInput(),
 		ActionRef: &gqlengine.ManifestReferenceInput{
 			Path: opts.InterfacePath,
 		},
@@ -84,85 +52,4 @@ func Create(ctx context.Context, opts CreateOptions, w io.Writer) (*CreateOutput
 		Action:    act,
 		Namespace: opts.Namespace,
 	}, nil
-}
-
-func askForInputParams() (*gqlengine.ActionInputData, error) {
-	gqlJSON, err := askForInputParameters()
-	if err != nil {
-		return nil, err
-	}
-
-	ti, err := askForInputTypeInstances()
-	if err != nil {
-		return nil, err
-	}
-
-	return &gqlengine.ActionInputData{
-		Parameters:    gqlJSON,
-		TypeInstances: ti,
-	}, nil
-}
-
-// TODO: ask only if input-parameters are defined, add support for JSON Schema
-func askForInputParameters() (*gqlengine.JSON, error) {
-	provideInput := false
-	askAboutTI := &survey.Confirm{Message: "Do you want to provide input parameters?", Default: false}
-	if err := survey.AskOne(askAboutTI, &provideInput); err != nil {
-		return nil, err
-	}
-
-	if !provideInput {
-		return nil, nil
-	}
-
-	rawInput := ""
-	prompt := &survey.Editor{Message: "Please type Action input parameters in YAML format"}
-	if err := survey.AskOne(prompt, &rawInput, survey.WithValidator(isYAML)); err != nil {
-		return nil, err
-	}
-
-	converted, err := yaml.YAMLToJSON([]byte(rawInput))
-	if err != nil {
-		return nil, err
-	}
-
-	gqlJSON := gqlengine.JSON(converted)
-	return &gqlJSON, nil
-}
-
-func askForInputTypeInstances() ([]*gqlengine.InputTypeInstanceData, error) {
-	provideTI := false
-	askAboutTI := &survey.Confirm{Message: "Do you want to provide input TypeInstances?", Default: false}
-	if err := survey.AskOne(askAboutTI, &provideTI); err != nil {
-		return nil, err
-	}
-
-	if !provideTI {
-		return nil, nil
-	}
-
-	editor := ""
-	prompt := &survey.Editor{
-		Message: "Please type Action input TypeInstance in YAML format",
-		Default: heredoc.Doc(`
-						typeInstances:
-						  - name: ""
-						    id: ""`),
-		AppendDefault: true,
-
-		HideDefault: true,
-	}
-	if err := survey.AskOne(prompt, &editor, survey.WithValidator(isYAML)); err != nil {
-		return nil, err
-	}
-
-	var resp struct {
-		TypeInstances []*gqlengine.InputTypeInstanceData `json:"typeInstances"`
-	}
-
-	if err := yaml.Unmarshal([]byte(editor), &resp); err != nil {
-		return nil, err
-	}
-
-	return resp.TypeInstances, nil
 }
