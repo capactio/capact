2a92837ccc61adfbcf116ca0781dc1be70f8537e
Mateusz Szostok <mateusz@capact.io>
2021-07-01 15:13:37 +0200

    Add fake info that Go code in investigation is generated

--- docs/investigation/backup-restore/runner/runner.go
+++ docs/investigation/backup-restore/runner/runner.go
@@ -1,4 +1,4 @@
-//nolint:golint
+// @generated - This was created as a part of investigation. We mark it as generate to exlude it from goreportcard to do not have missleading issues.:golint
 package main
 
 import (
--- docs/investigation/graph-db/dgraph/app/internal/interface_populator.go
+++ docs/investigation/graph-db/dgraph/app/internal/interface_populator.go
@@ -1,3 +1,4 @@
+// @generated - This was created as a part of investigation. We mark it as generate to exlude it from goreportcard to do not have missleading issues.:golint
 package internal
 
 import (
