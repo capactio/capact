9f71e36ca16da701a00b85a25e487c551d26bfd8
Mateusz Szostok <mateusz@capact.io>
2021-07-12 13:04:30 +0200

    kind

--- internal/cli/environment/create/kind_defaults.go
+++ internal/cli/environment/create/kind_defaults.go
@@ -2,16 +2,20 @@ package create
 
 import "sigs.k8s.io/kind/pkg/apis/config/v1alpha4"
 
-const KubeadmKindConfigPatches = `kind: InitConfiguration
+const kubeadmKindConfigPatches = `kind: InitConfiguration
 nodeRegistration:
   kubeletExtraArgs:
     node-labels: "ingress-ready=true"
 `
+
 const (
+	// KindDefaultNodeImage defines default Kubernetes image for a new kind cluster.
 	KindDefaultNodeImage   = "kindest/node:v1.19.1"
+	// KindDefaultClusterName defines default name for a new kind cluster.
 	KindDefaultClusterName = "kind-dev-capact"
 )
 
+// KindDefaultConfig holds kind cluster configuration suitable for Capact installation.
 var KindDefaultConfig = &v1alpha4.Cluster{
 	TypeMeta: v1alpha4.TypeMeta{
 		Kind:       "Cluster",
@@ -20,7 +24,7 @@ var KindDefaultConfig = &v1alpha4.Cluster{
 	Nodes: []v1alpha4.Node{
 		{
 			Role:                 v1alpha4.ControlPlaneRole,
-			KubeadmConfigPatches: []string{KubeadmKindConfigPatches},
+			KubeadmConfigPatches: []string{kubeadmKindConfigPatches},
 			ExtraPortMappings: []v1alpha4.PortMapping{
 				{
 					ContainerPort: 80,
