d3e788433f0771137d394173b0869fab02c01e69
Mateusz Szostok <mateusz@capact.io>
2021-07-28 15:33:57 +0200

    Always build infra images

--- hack/ci/setup-env.sh
+++ hack/ci/setup-env.sh
@@ -34,34 +34,9 @@ else
   echo "DOCKER_REPOSITORY=ghcr.io/capactio" >> "$GITHUB_ENV"
 fi
 
-function returnInfraMatrixIfNeeded() {
-  while read -r file; do
-    if [[ $file == hack/images/* ]]; then
-      # TODO: jinja2 is a Capact Action. Move it to a separate directory or create a new repo for it
-      echo 'INFRAS=name=matrix::{"include":[{"INFRA":"json-go-gen"},{"INFRA":"graphql-schema-linter"},{"INFRA":"jinja2"}]}'
-      break
-    fi
-  done <<< "$(gitChanges)"
-}
-
-function gitChanges() {
-  local DIFF
-  # See https://github.community/t/check-pushed-file-changes-with-git-diff-tree-in-github-actions/17220/10
-  if [ "$GITHUB_BASE_REF" ]; then
-    # Pull Request
-    git fetch origin "$GITHUB_BASE_REF" --depth=1
-    DIFF=$( git diff --name-only origin/"$GITHUB_BASE_REF" "$GITHUB_SHA" )
-  else
-    # Push
-    DIFF=$( git diff --name-only HEAD^ HEAD )
-  fi
-
-  echo "$DIFF"
-}
-
 # TODO: Read components to build in automated way, e.g. from directory structure
 cat <<EOT >>"$GITHUB_ENV"
 APPS=name=matrix::{"include":[{"APP":"gateway"},{"APP":"k8s-engine"},{"APP":"hub-js"},{"APP":"argo-runner"},{"APP":"helm-runner"},{"APP":"populator"},{"APP":"terraform-runner"},{"APP":"argo-actions"}]}
 TESTS=name=matrix::{"include":[{"TEST":"e2e"}]}
-$(returnInfraMatrixIfNeeded)
+INFRAS=name=matrix::{"include":[{"INFRA":"json-go-gen"},{"INFRA":"graphql-schema-linter"},{"INFRA":"jinja2"}]}
 EOT
