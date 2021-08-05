926517cc92f3680868fadf2e2b1ab821b987859b
Łukasz Oleś <lukaszoles@gmail.com>
2021-08-05 15:39:30 +0200

    lint

--- hack/images/merger/merger.sh
+++ hack/images/merger/merger.sh
@@ -4,11 +4,12 @@ SRC=${SRC:-"/yamls"}
 OUT=${OUT:-"/merged.yaml"}
 
 # prefix each file with its filename
-for filename in ${SRC}/*.yaml; do
+for filename in "${SRC}"/*.yaml; do
   filename=$(basename -- "$filename")
   prefix="${filename%.*}"
-  yq e -i "{\"${prefix}\": . }" ${SRC}/${filename}
+  yq e -i "{\"${prefix}\": . }" "${SRC}"/"${filename}"
 done
 
 # merge all yaml files into one
-yq ea '. as $item ireduce ({}; . * $item )' ${SRC}/*.yaml >${OUT}
+# shellcheck disable=SC2016
+yq ea '. as $item ireduce ({}; . * $item )' "${SRC}"/*.yaml >"${OUT}"
