4549d0d3715f52d2f754e64ed86605786016abfa
Damian Czaja <trojan295@protonmail.com>
2021-08-10 16:16:45 +0200

    increase cluster CPU limit to 10 and memory to 40GB. Run terraform fmt (#435)

--- hack/ci/terraform/google_compute_firewall.tf
+++ hack/ci/terraform/google_compute_firewall.tf
@@ -5,18 +5,18 @@ resource "google_compute_firewall" "gcf-default-allow-http-and-https" {
 
   allow {
     protocol = "tcp"
-    ports    = ["80","443"]
+    ports    = ["80", "443"]
   }
 }
 
 resource "google_compute_firewall" "gcf-allow-tcp" {
-  name    = "gke-allow-master-${var.cluster_name}"
-  project = var.project
-  network = google_compute_network.gcn_vpc.name
+  name          = "gke-allow-master-${var.cluster_name}"
+  project       = var.project
+  network       = google_compute_network.gcn_vpc.name
   source_ranges = ["172.16.10.0/28"]
 
   allow {
     protocol = "tcp"
-    ports    = ["443","10250", "80", "8443"]
+    ports    = ["443", "10250", "80", "8443"]
   }
 }
--- hack/ci/terraform/google_compute_subnetwork.tf
+++ hack/ci/terraform/google_compute_subnetwork.tf
@@ -1,17 +1,17 @@
-resource "google_compute_subnetwork" "gcs_compute_subnetwork"{
-  name   = var.google_compute_subnetwork_name
-  ip_cidr_range = var.google_compute_subnetwork_ip_cidr_range
-  project = var.project
-  region        = var.region
-  network       = google_compute_network.gcn_vpc.id
+resource "google_compute_subnetwork" "gcs_compute_subnetwork" {
+  name                     = var.google_compute_subnetwork_name
+  ip_cidr_range            = var.google_compute_subnetwork_ip_cidr_range
+  project                  = var.project
+  region                   = var.region
+  network                  = google_compute_network.gcn_vpc.id
   private_ip_google_access = true
   secondary_ip_range {
-    range_name = var.google_compute_subnetwork_secondary_ip_range_name1   
+    range_name    = var.google_compute_subnetwork_secondary_ip_range_name1
     ip_cidr_range = var.google_compute_subnetwork_secondary_ip_range_cidr1
   }
 
   secondary_ip_range {
-    range_name = var.google_compute_subnetwork_secondary_ip_range_name2  
+    range_name    = var.google_compute_subnetwork_secondary_ip_range_name2
     ip_cidr_range = var.google_compute_subnetwork_secondary_ip_range_cidr2
   }
 }
