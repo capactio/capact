7ecb88d30af121e1ea80e69bd8cf377f4d601ad9
Damian Czaja <trojan295@protonmail.com>
2021-08-24 17:11:30 +0200

    simplify

--- hack/make-release.sh
+++ hack/make-release.sh
@@ -2,6 +2,29 @@
 
 set -e
 
+release::update_helm_charts_version() {
+  local -r release_version="$1"
+  local -r deploy_dir=deploy/kubernetes/charts
+
+  for d in "${deploy_dir}"/*/ ; do
+    sed -i.bak "s/^version: .*/version: ${release_version}/g" "${d}/Chart.yaml"
+  done
+}
+
+release::update_cli_version() {
+  local -r release_version="$1"
+  sed -i.bak "s/Version = .*/Version = \"${release_version}\"/g" "internal/cli/info.go"
+}
+
+release::make_prepare_release_commit() {
+  local -r version="$1"
+  local -r branch="$2"
+
+  git add .
+  git commit -m "Prepare ${version} release"
+  git push origin "${branch}"
+}
+
 release::set_capact_images_in_charts() {
   local -r image_tag="$1"
   sed -i.bak "s/overrideTag: \"latest\"/overrideTag: \"${image_tag}\"/g" "deploy/kubernetes/charts/capact/values.yaml"
@@ -25,7 +48,7 @@ release::make_release_commit() {
 }
 
 # required inputs:
-# GIT_TAG_REF - Git tag ref in format 'refs/tags/vx.y.z.'
+# RELEASE_VERSION - new version in semver format: x.y.z
 [ -z "${RELEASE_VERSION}" ] && echo "Need to set RELEASE_VERSION" && exit 1;
 
 SOURCE_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
@@ -33,8 +56,11 @@ RELEASE_VERSION_MAJOR_MINOR="$(echo "${RELEASE_VERSION}" | sed -E 's/([0-9]+\.[0
 RELEASE_BRANCH="release-${RELEASE_VERSION_MAJOR_MINOR}"
 
 main() {
-  local -r capact_image_tag=$(git rev-parse --short HEAD | sed 's/.$//')
+  release::update_helm_charts_version "${RELEASE_VERSION}"
+  release::update_cli_version "${RELEASE_VERSION}"
+  release::make_prepare_release_commit "${RELEASE_VERSION}" "${SOURCE_BRANCH}"
 
+  local -r capact_image_tag=$(git rev-parse --short HEAD | sed 's/.$//')
   git checkout -B "${RELEASE_BRANCH}"
 
   release::set_capact_images_in_charts "${capact_image_tag}"
