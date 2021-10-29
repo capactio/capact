62b733432d265a81e699fb705fa038b219303e9c
Łukasz Oleś <lukaszoles@gmail.com>
2021-10-29 17:05:51 +0200

    Use correct struct to store Manifest Reference (#545)
    Fixes #536

---
+++ pkg/engine/k8s/policy/workflow_test.go
@@ -0,0 +1,77 @@
+package policy
+
+import (
+	"testing"
+
+	"capact.io/capact/pkg/sdk/apis/0.0.1/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestToPolicy(t *testing.T) {
+	// given
+	givenWorkflowPolicy := workflowPolicyWithAdditionalInput(map[string]interface{}{"a": map[string]interface{}{"enabled": false}})
+	expectedPolicy := policyWithAdditionalInput(map[string]interface{}{"a": map[string]interface{}{"enabled": false}})
+	// when
+	convertedPolicy, err := givenWorkflowPolicy.ToPolicy()
+	// then
+	assert.NoError(t, err)
+	assert.Equal(t, expectedPolicy, convertedPolicy)
+}
+
+func workflowPolicyWithAdditionalInput(input map[string]interface{}) WorkflowPolicy {
+	implementation := "cap.implementation.bitnami.postgresql.install"
+	return WorkflowPolicy{
+		Rules: WorkflowRulesList{
+			WorkflowRulesForInterface{
+				Interface: WorkflowInterfaceRef{
+					ManifestRef: &types.ManifestRefWithOptRevision{
+						Path: "cap.interface.database.postgresql.install",
+					},
+				},
+				OneOf: []WorkflowRule{
+					{
+						ImplementationConstraints: ImplementationConstraints{
+							Path: &implementation,
+						},
+						Inject: &WorkflowInjectData{
+							AdditionalParameters: []AdditionalParametersToInject{
+								{
+									Name:  "additional-parameters",
+									Value: input,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func policyWithAdditionalInput(input map[string]interface{}) Policy {
+	implementation := "cap.implementation.bitnami.postgresql.install"
+	return Policy{
+		Rules: RulesList{
+			RulesForInterface{
+				Interface: types.ManifestRefWithOptRevision{
+					Path: "cap.interface.database.postgresql.install",
+				},
+				OneOf: []Rule{
+					{
+						ImplementationConstraints: ImplementationConstraints{
+							Path: &implementation,
+						},
+						Inject: &InjectData{
+							AdditionalParameters: []AdditionalParametersToInject{
+								{
+									Name:  "additional-parameters",
+									Value: input,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
