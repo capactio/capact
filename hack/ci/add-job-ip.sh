88e827aa43bfcef469a8e4eaddb049a3904a5a81
Mateusz Kuziemko <mateusz.k@capact.io>
2021-12-07 22:46:59 +0100

    Migrate yq to V4

--- hack/ci/add-job-ip.sh
+++ hack/ci/add-job-ip.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2154
 
-sudo snap install yq --channel=v3/stable
+sudo snap install yq --channel=v4/stable
 IP_ADDED_JOB=$(printf "%s/32" "$(curl "${GET_IP_SERVICE}")")
-AUTHORIZED=$(gcloud container clusters describe "${TF_VAR_cluster_name}" --zone "${TF_VAR_region}" |yq r - 'masterAuthorizedNetworksConfig.cidrBlocks[*].cidrBlock')
+AUTHORIZED=$(gcloud container clusters describe "${TF_VAR_cluster_name}" --zone "${TF_VAR_region}" |yq e '.masterAuthorizedNetworksConfig.cidrBlocks[].cidrBlock' -)
 AUTHORIZED=$(echo "${AUTHORIZED}" | tr '\n' ',' | sed 's/^,//g;s/ //g')
 AUTHORIZED=$(printf "%s%s" "${AUTHORIZED}" "${IP_ADDED_JOB}" |sed s/^,//g)
 gcloud container clusters update "${TF_VAR_cluster_name}" --zone "${TF_VAR_region}" --enable-master-authorized-networks \
