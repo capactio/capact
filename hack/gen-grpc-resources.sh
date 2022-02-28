f51af881384260819ed9de6748bed11411ff3f3a
Paweł Kosiec <pawel@capact.io>
2022-02-28 09:43:31 +0100

    Implement Secret storage backend as gRPC server (#644)
    - Add Protocol Buffers schema
    - Generate gRPC client + server code from Protocol Buffers
    - Implement gRPC server for secret storage backend
        - Support initially just AWS Secrets Manager. Tested also `dotenv` provider. The `vault` provider needs to be tested
        - Add tests for the gRPC server with fake secret provider

---
+++ hack/gen-grpc-resources.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+#
+# This script generates gRPC + ProtoBuf Go code for client and server.
+#
+# Dependencies such as `protoc` binary can be installed on demand.
+
+# standard bash error handling
+set -o nounset # treat unset variables as an error and exit immediately.
+set -o errexit # exit immediately when a command fails.
+set -E         # needs to be set if we want the ERR trap
+
+CURRENT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+REPO_ROOT_DIR=$(cd "${CURRENT_DIR}/.." && pwd)
+TMP_DIR=$(mktemp -d)
+readonly CURRENT_DIR
+readonly REPO_ROOT_DIR
+readonly TMP_DIR
+
+SKIP_DEPS_INSTALLATION=${SKIP_DEPS_INSTALLATION:-true}
+
+# shellcheck source=./hack/lib/utilities.sh
+source "${CURRENT_DIR}/lib/utilities.sh" || { echo 'Cannot load CI utilities.' exit 1; }
+# shellcheck source=./hack/lib/const.sh
+source "${CURRENT_DIR}/lib/const.sh" || { echo 'Cannot load constant values.' exit 1; }
+
+cleanup() {
+  rm -rf "${TMP_DIR}"
+}
+
+trap cleanup EXIT
+
+# protoc uses different naming pattern for binary than what we have in the utilities.sh
+host::os() {
+  local host_os
+  case "$(uname -s)" in
+    Darwin)
+      host_os=osx
+      ;;
+    Linux)
+      host_os=linux
+      ;;
+    *)
+      echo "Unsupported host OS. Must be Linux or Mac OS X."
+      exit 1
+      ;;
+  esac
+  echo "${host_os}"
+}
+
+host::install::protoc() {
+  shout "Install the protoc ${STABLE_PROTOC_VERSION} locally to a tempdir..."
+  mkdir -p "${TMP_DIR}/bin"
+
+  export GOBIN="${TMP_DIR}/bin"
+  export PATH="${GOBIN}:${PATH}"
+
+  pushd "$TMP_DIR" >/dev/null
+
+  os=$(host::os)
+  arch=$(uname -m)
+  version_without_v=${STABLE_PROTOC_VERSION#"v"}
+  name="protoc-${version_without_v}-${os}-${arch}"
+
+  # download the release
+  curl -L -O "https://github.com/protocolbuffers/protobuf/releases/download/${STABLE_PROTOC_VERSION}/${name}.zip"
+
+  # extract the archive
+  unzip "${name}".zip
+
+  go install "google.golang.org/protobuf/cmd/protoc-gen-go@${STABLE_PROTOC_GEN_GO_VERSION}"
+  go install "google.golang.org/grpc/cmd/protoc-gen-go-grpc@${STABLE_PROTOC_GEN_GO_GRPC_VERSION}"
+
+  popd >/dev/null
+
+  echo -e "${GREEN}√ install protoc${NC}"
+}
+
+main() {
+  if [[ "${SKIP_DEPS_INSTALLATION}" == "false" ]]; then
+    host::install::protoc
+  else
+    echo "Skipping protoc installation cause SKIP_DEPS_INSTALLATION is set to true."
+  fi
+
+  shout "Generating Capact gRPC related resources..."
+
+  readonly apiPaths=(
+    "/pkg/hub/api/grpc"
+  )
+
+  for path in "${apiPaths[@]}"; do
+    echo "- Processing ${path}..."
+    pushd "${REPO_ROOT_DIR}$path" > /dev/null
+    protoc -I=. --go_out=. --go-grpc_out=. ./*.proto
+    popd > /dev/null
+  done
+
+  shout "Generation completed successfully."
+}
+
+main
---
+++ internal/secret-storage-backend/server.go
@@ -0,0 +1,416 @@
+package secretstoragebackend
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"capact.io/capact/internal/ptr"
+	pb "capact.io/capact/pkg/hub/api/grpc/storage_backend"
+	"github.com/pkg/errors"
+	tellercore "github.com/spectralops/teller/pkg/core"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// Context holds Secret storage backend specific parameters.
+type Context struct {
+	Provider string `json:"provider"`
+}
+
+var _ pb.StorageBackendServer = &Handler{}
+
+// Handler handles incoming requests to the Secret storage backend gRPC server.
+type Handler struct {
+	pb.UnimplementedStorageBackendServer
+
+	log *zap.Logger
+
+	providers map[string]tellercore.Provider
+}
+
+const (
+	lockedByField        = "locked_by"
+	firstResourceVersion = 1
+)
+
+var (
+	// NilRequestInputError describes an error with an invalid request.
+	NilRequestInputError = status.Error(codes.InvalidArgument, "request data cannot be nil")
+)
+
+// NewHandler returns new Handler.
+func NewHandler(log *zap.Logger, providers map[string]tellercore.Provider) *Handler {
+	return &Handler{
+		log:       log,
+		providers: providers,
+	}
+}
+
+// GetValue returns a value for a given TypeInstance. It returns nil as value if a given secret is not found.
+func (h *Handler) GetValue(_ context.Context, request *pb.GetValueRequest) (*pb.GetValueResponse, error) {
+	if request == nil {
+		return nil, NilRequestInputError
+	}
+
+	provider, err := h.getProviderFromContext(request.Context)
+	if err != nil {
+		return nil, err
+	}
+
+	key := h.storageKeyForTypeInstanceValue(provider, request.TypeinstanceId, request.ResourceVersion)
+	entry, err := h.getEntry(provider, key)
+	if err != nil {
+		return nil, err
+	}
+
+	if !entry.IsFound {
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("TypeInstance %q in revision %d was not found", request.TypeinstanceId, request.ResourceVersion))
+	}
+
+	return &pb.GetValueResponse{
+		Value: []byte(entry.Value),
+	}, nil
+}
+
+// GetLockedBy returns a locked by data for a given TypeInstance. It returns nil as value if a given secret is not found.
+func (h *Handler) GetLockedBy(_ context.Context, request *pb.GetLockedByRequest) (*pb.GetLockedByResponse, error) {
+	if request == nil {
+		return nil, NilRequestInputError
+	}
+
+	provider, err := h.getProviderFromContext(request.Context)
+	if err != nil {
+		return nil, err
+	}
+
+	key := tellercore.KeyPath{
+		Path: h.storagePathForTypeInstance(provider, request.TypeinstanceId),
+	}
+	entries, err := h.getEntriesForPath(provider, key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(entries) == 0 {
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("TypeInstance %q not found: secret from path %q is empty", request.TypeinstanceId, key.Path))
+	}
+
+	var lockedBy *string
+	for _, entry := range entries {
+		if entry.Key == lockedByField && entry.Value != "" {
+			lockedBy = ptr.String(entry.Value)
+		}
+	}
+
+	return &pb.GetLockedByResponse{
+		LockedBy: lockedBy,
+	}, nil
+}
+
+// OnCreate handles TypeInstance creation by creating secret in a given provider.
+func (h *Handler) OnCreate(_ context.Context, request *pb.OnCreateRequest) (*pb.OnCreateResponse, error) {
+	if request == nil {
+		return nil, NilRequestInputError
+	}
+
+	provider, err := h.getProviderFromContext(request.Context)
+	if err != nil {
+		return nil, err
+	}
+
+	key := h.storageKeyForTypeInstanceValue(provider, request.TypeinstanceId, firstResourceVersion)
+	if err := h.ensureSecretCanBeCreated(provider, key); err != nil {
+		return nil, err
+	}
+
+	err = h.putEntry(provider, key, request.Value)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.OnCreateResponse{}, nil
+}
+
+// OnUpdate handles TypeInstance update by updating secret in a given provider.
+func (h *Handler) OnUpdate(_ context.Context, request *pb.OnUpdateRequest) (*pb.OnUpdateResponse, error) {
+	if request == nil {
+		return nil, NilRequestInputError
+	}
+
+	provider, err := h.getProviderFromContext(request.Context)
+	if err != nil {
+		return nil, err
+	}
+
+	key := h.storageKeyForTypeInstanceValue(provider, request.TypeinstanceId, request.NewResourceVersion)
+
+	if err := h.ensureSecretCanBeUpdated(provider, key); err != nil {
+		return nil, err
+	}
+
+	err = h.putEntry(provider, key, request.NewValue)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.OnUpdateResponse{}, nil
+}
+
+// OnLock handles TypeInstance locking by setting a secret entry in a given provider.
+// It doesn't check whether a given TypeInstance is already locked, but overrides the value in place
+func (h *Handler) OnLock(_ context.Context, request *pb.OnLockRequest) (*pb.OnLockResponse, error) {
+	if request == nil {
+		return nil, NilRequestInputError
+	}
+
+	provider, err := h.getProviderFromContext(request.Context)
+	if err != nil {
+		return nil, err
+	}
+
+	err = h.ensureSecretIsNotLocked(provider, request.TypeinstanceId)
+	if err != nil {
+		return nil, err
+	}
+
+	key := h.storageKeyForLockedBy(provider, request.TypeinstanceId)
+
+	err = h.putEntry(provider, key, []byte(request.LockedBy))
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.OnLockResponse{}, nil
+}
+
+// OnUnlock handles TypeInstance unlocking by removing secret entry in a given provider.
+func (h *Handler) OnUnlock(_ context.Context, request *pb.OnUnlockRequest) (*pb.OnUnlockResponse, error) {
+	if request == nil {
+		return nil, NilRequestInputError
+	}
+
+	provider, err := h.getProviderFromContext(request.Context)
+	if err != nil {
+		return nil, err
+	}
+
+	key := tellercore.KeyPath{
+		Path: h.storagePathForTypeInstance(provider, request.TypeinstanceId),
+	}
+	err = h.ensureSecretCanBeUnlocked(provider, key)
+	if err != nil {
+		return nil, err
+	}
+
+	lockedByKey := h.storageKeyForLockedBy(provider, request.TypeinstanceId)
+	err = h.deleteEntry(provider, lockedByKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.OnUnlockResponse{}, nil
+}
+
+// OnDelete handles TypeInstance deletion by removing a secret in a given provider.
+// It checks whether a given TypeInstance is locked before doing such operation.
+func (h *Handler) OnDelete(_ context.Context, request *pb.OnDeleteRequest) (*pb.OnDeleteResponse, error) {
+	if request == nil {
+		return nil, NilRequestInputError
+	}
+
+	provider, err := h.getProviderFromContext(request.Context)
+	if err != nil {
+		return nil, err
+	}
+
+	key := tellercore.KeyPath{
+		Path: h.storagePathForTypeInstance(provider, request.TypeinstanceId),
+	}
+	err = h.ensureSecretCanBeDeleted(provider, key)
+	if err != nil {
+		return nil, err
+	}
+
+	err = provider.DeleteMapping(key)
+	if err != nil {
+		return nil, h.internalError(errors.Wrapf(err, "while deleting TypeInstance %q", request.TypeinstanceId))
+	}
+
+	return &pb.OnDeleteResponse{}, nil
+}
+
+func (h *Handler) getProviderFromContext(contextBytes []byte) (tellercore.Provider, error) {
+	var context Context
+	err := json.Unmarshal(contextBytes, &context)
+	if err != nil {
+		return nil, h.internalError(errors.Wrap(err, "while unmarshaling additional parameters"))
+	}
+
+	provider, ok := h.providers[context.Provider]
+	if !ok {
+		return nil, h.internalError(fmt.Errorf("missing loaded provider with name %q", context.Provider))
+	}
+
+	return provider, nil
+}
+
+func (h *Handler) getEntry(provider tellercore.Provider, key tellercore.KeyPath) (*tellercore.EnvEntry, error) {
+	h.log.Info("getting entry", zap.String("path", key.Path), zap.String("provider", provider.Name()))
+	entry, err := provider.Get(key)
+	if err != nil {
+		return nil, h.internalError(errors.Wrapf(err, "while getting value by key %q", key.Path))
+	}
+
+	return entry, nil
+}
+
+func (h *Handler) getEntriesForPath(provider tellercore.Provider, key tellercore.KeyPath) ([]tellercore.EnvEntry, error) {
+	h.log.Info("getting whole secret", zap.String("path", key.Path), zap.String("provider", provider.Name()))
+	entries, err := provider.GetMapping(key)
+	if err != nil {
+		return nil, h.internalError(errors.Wrapf(err, "while getting value by path %q", key.Path))
+	}
+
+	return entries, nil
+}
+
+func (h *Handler) putEntry(provider tellercore.Provider, key tellercore.KeyPath, value []byte) error {
+	h.log.Info("putting entry", zap.String("path", key.Path), zap.String("field", key.Field), zap.String("provider", provider.Name()))
+	err := provider.Put(key, string(value))
+	if err != nil {
+		return h.internalError(errors.Wrapf(err, "while putting value for key %q", key.Path))
+	}
+
+	return nil
+}
+
+func (h *Handler) deleteEntry(provider tellercore.Provider, key tellercore.KeyPath) error {
+	h.log.Info("deleting entry", zap.String("path", key.Path), zap.String("field", key.Field), zap.String("provider", provider.Name()))
+	err := provider.Delete(key)
+	if err != nil {
+		return h.internalError(errors.Wrapf(err, "while deleting %q for key %q", key.Field, key.Path))
+	}
+
+	return nil
+}
+
+func (h *Handler) storageKeyForTypeInstanceValue(provider tellercore.Provider, tiID string, tiResourceVersion uint32) tellercore.KeyPath {
+	return tellercore.KeyPath{
+		Path:  h.storagePathForTypeInstance(provider, tiID),
+		Field: strconv.Itoa(int(tiResourceVersion)),
+	}
+}
+
+func (h *Handler) storageKeyForLockedBy(provider tellercore.Provider, tiID string) tellercore.KeyPath {
+	return tellercore.KeyPath{
+		Path:  h.storagePathForTypeInstance(provider, tiID),
+		Field: lockedByField,
+	}
+}
+
+func (h *Handler) storagePathForTypeInstance(provider tellercore.Provider, tiID string) string {
+	// depending on provider there might be a different path format
+	// e.g. see https://github.com/SpectralOps/teller#google-secret-manager
+
+	var prefix string
+	switch provider.Name() {
+	case "dotenv":
+		prefix = "/tmp/"
+	default:
+		prefix = "/"
+	}
+
+	return fmt.Sprintf("%scapact/%s", prefix, tiID)
+}
+
+func (h *Handler) ensureSecretCanBeCreated(provider tellercore.Provider, key tellercore.KeyPath) error {
+	entries, err := h.getEntriesForPath(provider, key)
+	if err != nil {
+		return h.internalError(err)
+	}
+
+	if len(entries) != 0 {
+		return status.Error(codes.AlreadyExists, fmt.Sprintf("path %q in provider %q already exist", key.Path, provider.Name()))
+	}
+
+	return nil
+}
+
+func (h *Handler) ensureSecretCanBeUpdated(provider tellercore.Provider, key tellercore.KeyPath) error {
+	entries, err := h.getEntriesForPath(provider, key)
+	if err != nil {
+		return h.internalError(err)
+	}
+
+	if len(entries) == 0 {
+		return status.Error(codes.NotFound, fmt.Sprintf("path %q in provider %q not found", key.Path, provider.Name()))
+	}
+
+	for _, entry := range entries {
+		if entry.Key == lockedByField {
+			return h.typeInstanceLockedError(key.Path, entry.Value)
+		}
+		if entry.Key == key.Field {
+			return status.Error(codes.AlreadyExists, fmt.Sprintf("field %q for path %q in provider %q already exist", key.Field, key.Path, provider.Name()))
+		}
+	}
+
+	return nil
+}
+
+func (h *Handler) ensureSecretIsNotLocked(provider tellercore.Provider, typeInstanceID string) error {
+	key := h.storageKeyForLockedBy(provider, typeInstanceID)
+	entry, err := h.getEntry(provider, key)
+	if err != nil {
+		return h.internalError(errors.Wrapf(err, "while getting entry"))
+	}
+	if entry.IsFound && entry.Value != "" {
+		return h.typeInstanceLockedError(key.Path, entry.Value)
+	}
+
+	return nil
+}
+
+func (h *Handler) ensureSecretCanBeDeleted(provider tellercore.Provider, key tellercore.KeyPath) error {
+	entries, err := h.getEntriesForPath(provider, key)
+	if err != nil {
+		return h.internalError(err)
+	}
+
+	if len(entries) == 0 {
+		return status.Error(codes.NotFound, fmt.Sprintf("path %q in provider %q not found", key.Path, provider.Name()))
+	}
+
+	for _, entry := range entries {
+		if entry.Key != lockedByField {
+			continue
+		}
+		return h.typeInstanceLockedError(key.Path, entry.Value)
+	}
+
+	return nil
+}
+
+func (h *Handler) ensureSecretCanBeUnlocked(provider tellercore.Provider, key tellercore.KeyPath) error {
+	entries, err := h.getEntriesForPath(provider, key)
+	if err != nil {
+		return h.internalError(err)
+	}
+
+	if len(entries) == 0 {
+		return status.Error(codes.NotFound, fmt.Sprintf("path %q in provider %q not found", key.Path, provider.Name()))
+	}
+
+	return nil
+}
+
+func (h *Handler) internalError(err error) error {
+	return status.Error(codes.Internal, err.Error())
+}
+
+func (h *Handler) typeInstanceLockedError(path, lockedByValue string) error {
+	return status.Error(codes.FailedPrecondition, fmt.Sprintf("typeInstance locked: path %q contains %q property with value %q", path, lockedByField, lockedByValue))
+}
