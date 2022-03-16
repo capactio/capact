30e59523b52c0ad9e9acd49314ee00ae443bbce8
Pawel Kosiec <pawel@capact.io>
2022-03-16 15:56:25 +0100

    Add a sample method for both handlers

--- internal/helm-storage-backend/release.go
+++ internal/helm-storage-backend/release.go
@@ -1,6 +1,8 @@
 package helmstoragebackend
 
 import (
+	"context"
+
 	pb "capact.io/capact/pkg/hub/api/grpc/storage_backend"
 	"go.uber.org/zap"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
@@ -20,4 +22,12 @@ func NewReleaseHandler(log *zap.Logger, helmCfgFlags *genericclioptions.ConfigFl
 	return &ReleaseHandler{
 		log: log,
 	}
+}
+
+// GetValue returns a value for a given TypeInstance.
+func (h *ReleaseHandler) GetValue(_ context.Context, _ *pb.GetValueRequest) (*pb.GetValueResponse, error) {
+	h.log.Info("Getting value")
+	return &pb.GetValueResponse{
+		Value: []byte(`{"handler": "release"}`),
+	}, nil
 }
