e3576c5cb4e24b277517e59acde353214880d1c3
Błażej Małecki <malecki.bla@gmail.com>
2022-05-16 10:03:25 +0200

    fixup! Add support for jsonpath to the CLI

--- internal/cli/printer/jsonpath.go
+++ internal/cli/printer/jsonpath.go
@@ -9,7 +9,7 @@ import (
 type JSONPath struct{}
 
 func (p *JSONPath) Print(in interface{}, w io.Writer) error {
-	printer, err := printers.NewJSONPathPrinter(".[0].latestResourceVersion.spec.value") // TODO Move somewhere else
+	printer, err := printers.NewJSONPathPrinter("{[0].backend.id}") // TODO Move somewhere else
 	if err != nil {
 		return err
 	}
