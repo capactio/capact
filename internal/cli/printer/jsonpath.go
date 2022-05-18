f55e4637922789649d2c917e90c94d5a54b5c236
Błażej Małecki <malecki.bla@gmail.com>
2022-05-18 08:44:15 +0200

    fixup! Add support for jsonpath to the CLI

--- internal/cli/printer/jsonpath.go
+++ internal/cli/printer/jsonpath.go
@@ -6,10 +6,12 @@ import (
 	"k8s.io/cli-runtime/pkg/printers"
 )
 
+// JSONPath prints selected data from JSON.
 type JSONPath struct {
 	printer *printers.JSONPathPrinter
 }
 
+// Print executes k8s JSONPath printer to write to given writer selected data from JSON.
 func (p *JSONPath) Print(in interface{}, w io.Writer) error {
 	p.printer.EnableJSONOutput(true)
 
